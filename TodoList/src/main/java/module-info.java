d8751d4b7fdb83a10013981e7ea38210e10657da
safaa_elhirch <elhirchsafaa7@gmail.com>
2025-05-10 13:34:23 +0100

    Delete TodoList directory

--- TodoList/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.todolist {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.todolist to javafx.fxml;
-    exports com.example.todolist;
-}
